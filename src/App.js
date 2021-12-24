0e38ee7a5e2e54dd7b8287987cf6b4996aadd7a9
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-12-24 14:02:29 +0300

    good

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [userInfo, setUserInfo] = useState({
+    user: '',
+    rememberMe: false
+  })
+
+  useEffect(() => {
+    const rememberMe = localStorage.getItem('rememberMe') === 'true';
+    const user = rememberMe ? localStorage.getItem('user') : '';
+    setUserInfo({ user, rememberMe });
+  }, [])
+
+  const handleChange = (event) => {
+    const input = event.target;
+    const value = input.type === 'checkbox' ? input.checked : input.value;
+
+    setUserInfo(prev => ({ ...prev, [input.name]: value }));
+  };
+
+  const handleFormSubmit = () => {
+    const { user, rememberMe } = userInfo;
+    localStorage.setItem('rememberMe', rememberMe);
+    localStorage.setItem('user', rememberMe ? user : '');};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleFormSubmit}>
+      <label>
+        User: <input name="user" value={userInfo.user} onChange={handleChange}/>
+      </label>
+      <label>
+        <input name="rememberMe" checked={userInfo.rememberMe} onChange={handleChange} type="checkbox"/> Remember me
+      </label>
+      <button type="submit">Sign In</button>
+      </form>
     </div>
   );
 }
